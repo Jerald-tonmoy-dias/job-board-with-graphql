e36e88943da47f64e80597db6f335b13d47444e5
Jerald Tonmoy Dias <tonmoyjareld@gmail.com>
2025-12-18 18:45:05 +0600

    initial setup

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,35 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
 import express from "express";
+import cors from "cors";
+import cookiePerser from "cookie-parser";
 
 const app = express();
 
+const server = new ApolloServer({
+  typeDefs: `
+  type Query {
+  hello:String
+  }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => "hello world"
+    }
+  }
+});
+
 async function startServer() {
+  await server.start();
+
+  app.use("/graphql", cors({
+    origin: "http://localhost:3000",
+    credentials: true
+  }),
+    cookiePerser(),
+    express.json(),
+    expressMiddleware(server));
+
   app.listen(4000, () => {
     console.log("Server is running on http://localhost:4000/graphql");
   });
